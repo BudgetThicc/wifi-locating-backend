83827c3f30512409bb685fd215b2b9dae0eb6c2d
Winning12 <wenhao_jun@outlook.com>
2019-03-15 20:59:59 +0800

    update nacigation

--- src/main/java/cloud/model/classroom/ClassroomController.java
+++ src/main/java/cloud/model/classroom/ClassroomController.java
@@ -155,7 +155,7 @@ public class ClassroomController extends BaseController {
     }
 
     @PostMapping(value = { "/classroom/navigation" })
-    public Result getNavigation(@PathVariable("startPoint") String startPoint,@PathVariable("endPoint") String endPoint) {
+    public Result getNavigation(@RequestParam("startPoint") String startPoint,@RequestParam("endPoint") String endPoint) {
         Map map=new Map("./RoomInformation.csv","./EdgeInformation.csv",16);
         int[] res=map.dijkstra(Integer.parseInt(startPoint),Integer.parseInt(endPoint));
         return new Result("SUCCESS", "Get classroom Navigation", res);
